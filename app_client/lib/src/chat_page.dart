e21b4ccfa5590ec69c16c99ef3acb685531891d3
Jorge Carlos <skay_1994@yahoo.com.br>
2021-04-01 14:38:19 -0300

    Updated ChatPage

--- app_client/lib/src/chat_page.dart
+++ app_client/lib/src/chat_page.dart
@@ -1,4 +1,8 @@
+import 'package:common/common.dart';
 import 'package:flutter/material.dart';
+import 'package:rx_notifier/rx_notifier.dart';
+
+import 'chat_controller.dart';
 
 class ChatPage extends StatefulWidget {
   final String name;
@@ -12,6 +16,14 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  ChatController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = ChatController(widget.name, widget.room);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,14 +35,31 @@ class _ChatPageState extends State<ChatPage> {
           height: double.infinity,
           child: Column(
             children: [
-              Expanded(
-                child: ListView.builder(
-                  itemBuilder: (_, id) {
-                    return ListTile(title: Text('${id}'));
-                  },
-                ),
-              ),
+              RxBuilder(builder: (_) {
+                return Expanded(
+                  child: ListView.builder(
+                    itemCount: controller.listEvents.length,
+                    itemBuilder: (_, id) {
+                      final event = controller.listEvents[id];
+
+                      if (event.type == SocketType.enter_room) {
+                        return ListTile(
+                            title: Text('${event.name} entrou na sala'));
+                      } else if (event.type == SocketType.leave_room) {
+                        return ListTile(
+                            title: Text('${event.name} saiu da sala'));
+                      }
+
+                      return ListTile(
+                        title: Text(event.name),
+                        subtitle: Text(event.text),
+                      );
+                    },
+                  ),
+                );
+              }),
               TextField(
+                controller: controller.textController,
                 decoration: InputDecoration(
                   border: OutlineInputBorder(),
                   hintText: 'Type your Text',
@@ -39,5 +68,11 @@ class _ChatPageState extends State<ChatPage> {
             ],
           )),
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
   }
 }
