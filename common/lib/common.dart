ecf70a7d2e585a77e57b6b3ae8cb436b9e7bbba4
Jorge Carlos <skay_1994@yahoo.com.br>
2021-03-31 21:11:37 -0300

    Created SocketEvent and SocketType

--- common/lib/common.dart
+++ common/lib/common.dart
@@ -1,8 +1,29 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library common;
 
-export 'src/common_base.dart';
+class SocketEvent {
+  final String name;
+  final String room;
+  final String text;
+  final SocketType type;
 
-// TODO: Export any libraries intended for clients of this package.
+  SocketEvent(
+      {required this.name,
+      required this.room,
+      this.text = '',
+      required this.type});
+
+  Map toJson() {
+    return {'name': name, 'room': room, 'text': text, 'type': type.toString()};
+  }
+
+  factory SocketEvent.toJson(Map json) {
+    return SocketEvent(
+        name: json['name'],
+        room: json['room'],
+        text: json['text'],
+        type: SocketType.values
+            .firstWhere((element) => element.toString() == json['type']));
+  }
+}
+
+enum SocketType { enter_room, leave_room, message }
