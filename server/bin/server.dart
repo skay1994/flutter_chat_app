ace166845457dff0c54087f46e1a0ee4be5005a4
Jorge Carlos <skay_1994@yahoo.com.br>
2021-03-31 21:11:06 -0300

    Starting with socket.io

--- server/bin/server.dart
+++ server/bin/server.dart
@@ -1,3 +1,37 @@
+import 'dart:io';
+
+import 'package:common/common.dart';
+import 'package:socket_io/socket_io.dart';
+
 void main(List<String> arguments) {
-  print('Hello world!');
+  final server = Server();
+  server.on('connection', (client) {
+    onConnection(client);
+  });
+
+  server.listen(Platform.environment['PORT'] ?? 3000);
+}
+
+void onConnection(Socket socket) {
+  socket.on('enter_room', (data) {
+    final name = data['name'];
+    final room = data['room'];
+
+    socket.join(room);
+    socket.to(room).broadcast.emit(
+        'message',
+        SocketEvent(name: name, room: room, type: SocketType.enter_room)
+            .toJson());
+
+    socket.on('disconnect', (_) {
+      socket.to(room).broadcast.emit(
+          'message',
+          SocketEvent(name: name, room: room, type: SocketType.leave_room)
+              .toJson());
+    });
+
+    socket.on('message', (data) {
+      socket.to(room).broadcast.emit('message', SocketEvent.toJson(data));
+    });
+  });
 }
