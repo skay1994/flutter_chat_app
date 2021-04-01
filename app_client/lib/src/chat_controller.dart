a346c0bbb0bc7fc2613f00876fca94c6c587e929
Jorge Carlos <skay_1994@yahoo.com.br>
2021-04-01 14:37:56 -0300

    Updated ChatController

--- app_client/lib/src/chat_controller.dart
+++ app_client/lib/src/chat_controller.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/cupertino.dart';
+import 'package:rx_notifier/rx_notifier.dart';
 import 'package:socket_io_client/socket_io_client.dart';
 import 'package:common/common.dart';
 
@@ -5,21 +7,39 @@ class ChatController {
   Socket socket;
   final String name;
   final String room;
+  final listEvents = RxList<SocketEvent>();
+  final textController = TextEditingController();
 
   ChatController(this.name, this.room) {
     _init();
   }
 
   void _init() {
-    socket = io(
-        'http://localhost:3000',
-        OptionBuilder().setTransports(['websocket']) // for Flutter or Dart VM
-            .build());
+    socket = io(API_URL, OptionBuilder().setTransports(['websocket']).build());
     socket.connect();
     socket.onConnect(
         (_) => socket.emit('enter_room', {'name': name, 'room': room}));
     socket.on('message', (data) {
       final event = SocketEvent.toJson(data);
+      listEvents.add(event);
     });
+  }
+
+  void send() {
+    final event = SocketEvent(
+      name: name,
+      room: room,
+      text: textController.text,
+      type: SocketType.message,
+    );
+    socket.emit('message', event.toJson());
+    textController.clear();
+    listEvents.add(event);
+  }
+
+  void dispose() {
+    socket.clearListeners();
+    socket.dispose();
+    textController.dispose();
   }
 }
